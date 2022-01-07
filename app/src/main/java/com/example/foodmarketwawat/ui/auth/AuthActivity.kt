4370e7b497f6f03e6d1f7f918c39e9d18d5c9cef
Wawat Prigala <wawatprigala00@gmail.com>
2022-01-07 09:30:08 +0700

    auth navigation

--- app/src/main/java/com/example/foodmarketwawat/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/foodmarketwawat/ui/auth/AuthActivity.kt
@@ -2,11 +2,23 @@ package com.example.foodmarketwawat.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
 import com.example.foodmarketwawat.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val pageRequest = intent.getIntExtra("page_request", 0)
+        if (pageRequest == 2) {
+            val navOptios = NavOptions.Builder()
+                .setPopUpTo(R.id.fragmentSignIn, true)
+                .build()
+
+            Navigation.findNavController(findViewById(R.id.authHostFragment))
+                .navigate(R.id.action_signup, null, navOptios)
+        }
     }
 }
--- app/src/main/java/com/example/foodmarketwawat/ui/auth/signin/SigninFragment.kt
+++ app/src/main/java/com/example/foodmarketwawat/ui/auth/signin/SigninFragment.kt
@@ -1,11 +1,14 @@
 package com.example.foodmarketwawat.ui.auth.signin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.foodmarketwawat.R
+import com.example.foodmarketwawat.ui.auth.AuthActivity
+import kotlinx.android.synthetic.main.fragment_signin.*
 
 class SigninFragment : Fragment() {
 
@@ -16,6 +19,18 @@ class SigninFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signin, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnSignup.setOnClickListener {
+            val signup = Intent(activity, AuthActivity::class.java)
+            signup.putExtra("page_request", 2)
+
+            startActivity(signup)
+        }
+
     }
 
 }
