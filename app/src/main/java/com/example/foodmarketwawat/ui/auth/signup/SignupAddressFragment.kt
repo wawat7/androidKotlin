ae7e82ba98f7591be248ae3c75b3be96d86b297d
Wawat Prigala <wawatprigala00@gmail.com>
2022-01-07 10:19:53 +0700

    finish slicing auth navigation

--- app/src/main/java/com/example/foodmarketwawat/ui/auth/signup/SignupAddressFragment.kt
+++ app/src/main/java/com/example/foodmarketwawat/ui/auth/signup/SignupAddressFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.foodmarketwawat.R
+import com.example.foodmarketwawat.ui.auth.AuthActivity
+import kotlinx.android.synthetic.main.fragment_signup_address.*
 
 class SignupAddressFragment : Fragment() {
 
@@ -16,6 +19,18 @@ class SignupAddressFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signup_address, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnSignupNow.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_signup_success, null)
+
+            (activity as AuthActivity).toolbarSignUpSuccess()
+
+        }
     }
 
 }
--- app/src/main/java/com/example/foodmarketwawat/ui/auth/signup/SignupFragment.kt
+++ app/src/main/java/com/example/foodmarketwawat/ui/auth/signup/SignupFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.foodmarketwawat.R
+import com.example.foodmarketwawat.ui.auth.AuthActivity
+import kotlinx.android.synthetic.main.fragment_signup.*
 
 class SignupFragment : Fragment() {
 
@@ -16,6 +19,17 @@ class SignupFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signup, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnContinue.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_signup_address, null)
+
+            (activity as AuthActivity).toolbarSignUpAddress()
+        }
     }
 
 }
